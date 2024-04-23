188cd0da6779348e2e9f0eb522f41f4e596fc660
TheOddlySeagull <theoddlyseagull00@gmail.com>
2024-04-23 10:33:23 +0200

    JSON communication front-end et back-end - #7: Ajout touche echap pour fermer un form (corrigé :x)

--- front/js/formulaire.js
+++ front/js/formulaire.js
@@ -91,6 +91,8 @@ function generateFormFromJson(card) {
             document.removeEventListener('keydown', closeForm);
         }
     }
+    
+    document.addEventListener('keydown', closeForm);
 
     // We add an event listener to the form
     formElement.addEventListener('submit', function(event) {
