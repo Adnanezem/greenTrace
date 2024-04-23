587766679fc79cb7f060bbe767dd056a9358bd2f
TheOddlySeagull <theoddlyseagull00@gmail.com>
2024-04-23 11:21:36 +0200

    JSON communication front-end et back-end - #7: Suppression d'une fonction inutile

--- front/js/formulaire.js
+++ front/js/formulaire.js
@@ -3,7 +3,7 @@ var COMPUTE_FORM_BACKEND_ENDPOINT = 'https://192.168.75.79/back_test/carbon/comp
 
 
 // function to generate the field form from a json file
-function generateFormFromJson(card) {
+function generateFormFromJson(card, modify = false) {
     
     // We create a floating div to contain the form
     let form = document.createElement('div');
@@ -150,7 +150,7 @@ function generateFormFromJson(card) {
         button.addEventListener('click', function() {
             console.log('Modifier button clicked');
             // Open the form
-            modifyFormFromJson(card);
+            generateFormFromJson(card, true);
         });
         div.appendChild(button);
 
@@ -219,186 +219,6 @@ function generateCardsFromJson() {
         });
 }
 
-// Function to modify the form
-function modifyFormFromJson(card) {
-    // We create a floating div to contain the form
-    let form = document.createElement('div');
-    form.className = 'floating_form';
-
-    // We create a form element
-    let formElement = document.createElement('form');
-    form.appendChild(formElement);
-
-    // We create a fieldset element
-    let fieldset = document.createElement('fieldset');
-    formElement.appendChild(fieldset);
-
-    // We create a legend element
-    let legend = document.createElement('legend');
-    legend.textContent = 'Formulaire';
-    fieldset.appendChild(legend);
-
-    // We create a div element to contain the fields
-    let fields = document.createElement('div');
-    fields.className = 'form_field_container';
-    fieldset.appendChild(fields);
-
-    // We loop through the fields
-    card.fields.forEach(field => {
-        // We create a div element to contain the field
-        let fieldDiv = document.createElement('div');
-        fieldDiv.className = 'form_field';
-        fields.appendChild(fieldDiv);
-
-        // We create a label element
-        let label = document.createElement('label');
-        label.textContent = field.name;
-        fieldDiv.appendChild(label);
-
-        // We switch on the field type
-        switch (field.type) {
-            case 'number input':
-                // We create an input element
-                let number_input = document.createElement('input');
-                number_input.type = 'number';
-                number_input.name = field.name;
-                number_input.placeholder = field.unit;
-                fieldDiv.appendChild(number_input);
-                break;
-            case 'scrolllist':
-                // We create a select element
-                let select = document.createElement('select');
-                select.name = field.name;
-                field.options.forEach(option => {
-                    let optionElement = document.createElement('option');
-                    optionElement.value = option;
-                    optionElement.textContent = option;
-                    select.appendChild(optionElement);
-                });
-                fieldDiv.appendChild(select);
-                break;
-            case 'time':
-                // We create an input element
-                let time_input = document.createElement('input');
-                time_input.type = 'time';
-                time_input.name = field.name;
-                fieldDiv.appendChild(time_input);
-                break;
-            default:
-                console.error('Unknown field type: ' + field.type);
-                break;
-        }
-
-
-    });
-
-    // We create a button element
-    let button = document.createElement('button');
-    button.textContent = 'Envoyer';
-    formElement.appendChild(button);
-
-    // We add a remove button (removes the card from localStorage and the card list)
-    let removeButton = document.createElement('button');
-    removeButton.textContent = 'Supprimer';
-    formElement.appendChild(removeButton);
-
-    // We append the form to the body
-    document.body.appendChild(form);
-
-    // If user presses the "esc" key, we close the form, and remove the event listener
-    let closeForm = function(event) {
-        if (event.key === 'Escape') {
-            form.remove();
-            document.removeEventListener('keydown', closeForm);
-        }
-    }
-
-    document.addEventListener('keydown', closeForm);
-
-    // We add an event listener to the form
-    formElement.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        //Check if the form is filled
-        if (!check_form_filled(form)) {
-            alert('Please fill all the fields');
-            return;
-        }
-
-        // check the type of the card
-        console.log(card.type);
-        let data = {};
-        card.fields.forEach(field => {
-            data[field.name] = formElement.querySelector('[name="' + field.name + '"]').value;
-        });
-        console.log(data);
-
-        // Load the "cardSelection" from localStorage
-        let cardSelection = JSON.parse(localStorage.getItem('cardSelection')) || [];
-
-        // Add the new card to the "cardSelection"
-        cardSelection.push(data);
-
-        // We close the form
-        form.remove();
-
-        // We display a success message
-        let successMessage = document.createElement('div');
-        successMessage.textContent = 'Formulaire envoyé avec succès';
-        document.body.appendChild(successMessage);
-        setTimeout(() => {
-            successMessage.remove();
-        }, 3000);
-
-        let cardListUser = document.getElementById('cardListUser'); // Get the user's card list
-
-        // Clone the card and change the button text to "Modifier"
-        let cardElement = document.createElement('div');
-        cardElement.className = 'card';
-
-        let div = document.createElement('div');
-
-        let h3 = document.createElement('h3');
-        h3.textContent = card.name;
-        div.appendChild(h3);
-
-        let p = document.createElement('p');
-        p.textContent = card.description;
-        div.appendChild(p);
-
-        let button = document.createElement('button');
-        button.textContent = 'Modifier';
-        button.addEventListener('click', function() {
-            console.log('Modifier button clicked');
-            // Open the form
-            modifyFormFromJson(card);
-        });
-        div.appendChild(button);
-
-        cardElement.appendChild(div);
-
-        cardListUser.appendChild(cardElement);
-
-        // Save the card to localStorage
-        localStorage.setItem('cardSelection', JSON.stringify(cardSelection));
-    });
-
-    // We add an event listener to the remove button
-    removeButton.addEventListener('click', function() {
-        // Load the "cardSelection" from localStorage
-        let cardSelection = JSON.parse(localStorage.getItem('cardSelection')) || [];
-
-        // Remove the card from the "cardSelection"
-        cardSelection = cardSelection.filter(cardItem => cardItem.name !== card.name);
-
-        // Save the new "cardSelection" to localStorage
-        localStorage.setItem('cardSelection', JSON.stringify(cardSelection));
-
-        // Remove the card from the card list
-        form.remove();
-    });
-}
-
 // Function to load the saved cards from localStorage
 function loadSavedCards() {
     let cardSelection = JSON.parse(localStorage.getItem('cardSelection')) || [];
@@ -421,7 +241,7 @@ function loadSavedCards() {
         button.addEventListener('click', function() {
             console.log('Modifier button clicked');
             // Open the form
-            generateFormFromJson(card);
+            generateFormFromJson(card, true);
         });
         div.appendChild(button);
 
