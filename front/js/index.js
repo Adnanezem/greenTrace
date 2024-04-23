e352fff874b9c49719be69897c75c33d7d03b3c7
Munoz Mateo <mateo.munoz@etu.univ-lyon1.fr>
2024-04-23 16:06:39 +0200

    Développement du front - #4 : Logout devient un bouton dans la page profil.

--- front/js/index.js
+++ front/js/index.js
@@ -14,16 +14,13 @@ function isConnected() {
     console.log(document)
     const profilNav = document.querySelector('#profilNav')
     const loginNav = document.querySelector('#loginNav')
-    const logoutNav = document.querySelector('#logoutNav');
     console.log('elem profil: ', profilNav)
     console.log('elem login: ', loginNav)
     if(sessionStorage.getItem("jwt") === null) {
         profilNav.style.display = 'none';
-        logoutNav.style.display = 'none';
         loginNav.style.display = 'flex';
     } else { // on est connecté
         profilNav.style.display = 'flex';
-        logoutNav.style.display = 'flex';
         loginNav.style.display = 'none';
     }
 }
