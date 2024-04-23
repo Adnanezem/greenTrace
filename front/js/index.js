33c80c62722ca75e6265cf3a15e59d9d02aaff8e
Munoz Mateo <mateo.munoz@etu.univ-lyon1.fr>
2024-04-23 15:55:46 +0200

    Développement du front - #4 : Ajout des modifications nécessaire à la déconnexion dans le front (html).

--- front/js/index.js
+++ front/js/index.js
@@ -14,13 +14,16 @@ function isConnected() {
     console.log(document)
     const profilNav = document.querySelector('#profilNav')
     const loginNav = document.querySelector('#loginNav')
+    const logoutNav = document.querySelector('#logoutNav');
     console.log('elem profil: ', profilNav)
     console.log('elem login: ', loginNav)
     if(sessionStorage.getItem("jwt") === null) {
         profilNav.style.display = 'none';
+        logout.style.display = 'none';
         loginNav.style.display = 'flex';
     } else { // on est connecté
         profilNav.style.display = 'flex';
+        logout.style.display = 'flex';
         loginNav.style.display = 'none';
     }
 }
