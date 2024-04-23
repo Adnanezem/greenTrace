1b24d9e6d0d5d167c8e8a24beeac6563086157b1
Munoz Mateo <mateo.munoz@etu.univ-lyon1.fr>
2024-04-23 11:35:19 +0200

    Développement du front - #4 : Modification pour prise en compte de la page profil.

--- front/js/config.js
+++ front/js/config.js
@@ -1,5 +1,7 @@
 var CONTACT_BACKEND_ENDPOINT = 'https://192.168.75.79/back_test/contact';
 var LOGIN_BACKEND_ENDPOINT = 'https://192.168.75.79/back_test/users/login';
 var SIGNUP_BACKEND_ENDPOINT = 'https://192.168.75.79/back_test/users/register';
+var COMPUTE_FORM_BACKEND_ENDPOINT = 'https://192.168.75.79/back_test/carbon/compute';
+
 
 console.log('config.js loaded');
