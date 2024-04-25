34fc37cb4a89b69292d3765ea50398c713bfedcc
Munoz Mateo <mateo.munoz@etu.univ-lyon1.fr>
2024-04-25 15:42:06 +0200

    JSON communication front-end et back-end - #7 : Récupération de l'hitorique utilisateur.

--- front/js/index.js
+++ front/js/index.js
@@ -1,4 +1,4 @@
-
+var CARBON_BACKEND_ENDPOINT = 'https://192.168.75.79/back_test/carbon/';
 
 function loadPage() {
     $(document).ready(function() {
@@ -6,6 +6,9 @@ function loadPage() {
         var page = window.location.search.substring(1).split('=')[1] || 'accueil';
         $('#content').load(`../html/${page}.html`, function() {
             isConnected();
+            if(page === 'profil') {
+                loadCarbonHistory();
+            }
         });
     });
 }
@@ -21,6 +24,33 @@ function isConnected() {
         profilNav.style.display = 'flex';
         loginNav.style.display = 'none';
     }
+}
+
+function loadCarbonHistory() {
+    const headers = new Headers();
+    headers.append("Authorization", sessionStorage.getItem("jwt"));
+    headers.append("U-Login", sessionStorage.getItem("U-Login"));
+    fetch(CARBON_BACKEND_ENDPOINT, {
+        method: 'GET',
+        headers: headers,
+    }).then(response => {
+        if (response.ok) {
+            console.log('Response: ', response);
+            return response.json();
+        } else {
+            console.log('Response: ', response);
+            // Hide processing message
+            toggleProcessingMessage(false);
+            //stay on the same page
+            throw new Error("Erreur lors de la déconnexion.")
+        }
+    }).then(json => {
+        const bilanSection = document.querySelector('#historiqueBilans')
+        // document.createElement
+        console.log(json);
+    }).catch(err => {
+        serverError(err);
+    });
 }
 
 loadPage();
