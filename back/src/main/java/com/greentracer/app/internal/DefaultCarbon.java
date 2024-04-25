d17388dd71a81b350eff0d64ed950786cdf93960
adnane <adnane.zemouri@etu.univ-lyon1.fr>
2024-04-25 18:40:39 +0200

    correction d'erreurs checkstyles

--- back/src/main/java/com/greentracer/app/internal/DefaultCarbon.java
+++ back/src/main/java/com/greentracer/app/internal/DefaultCarbon.java
@@ -53,42 +53,45 @@ public class DefaultCarbon {
                         String transportType = JSONUtils.getStringField(node, "transport type");
                         switch (transportType) {
                             case "Trajet en voiture":
-                            /*
-                             * https://calculis.net/co2
-                             */
-                            if(!fuel.isBlank()) { 
-                                if (fuel.equals("Diesel")) {
-                                    // Calcul pour voiture diesel
-                                    resultat += 160 * Integer.parseInt(distance);
+                                /*
+                                 * https://calculis.net/co2
+                                 */
+                                if(!fuel.isBlank()) {
+                                    if (fuel.equals("Diesel")) {
+                                        // Calcul pour voiture diesel
+                                        resultat += 160 * Integer.parseInt(distance);
+                                    }
+                                    if (fuel.equals("electric")) {
+                                        // Calcul pour voiture electric
+                                        resultat += 70 * Integer.parseInt(distance);
+                                    }
+                                    if (fuel.equals("gasoline")) {
+                                        // Calcul pour voiture gasoline
+                                        resultat += 180 * Integer.parseInt(distance);
+                                    }
                                 }
-                                if (fuel.equals("electric")) {
-                                    // Calcul pour voiture electric
-                                    resultat += 70 * Integer.parseInt(distance);
-                                }
-                                if (fuel.equals("gasoline")) {
-                                    // Calcul pour voiture gasoline
-                                    resultat += 180 * Integer.parseInt(distance);
-                                }
-                            }
                             break; 
                             case "Trajet en bus":
                             /*
                              * https://ekwateur.fr/blog/enjeux-environnementaux/empreinte-carbone-bus/
                              */
-                            if(!fuel.isBlank()) { 
-                                
-                                if(!fuel.isBlank()) { 
-                                    if (fuel.equals("Diesel")) {
-                                        // Calcul pour bus diesel
-                                        resultat += 1100 * Integer.parseInt(distance);
-                                    }
-                                    if (fuel.equals("electric")) {
-                                        // Calcul pour bus electric
-                                        resultat += 200 * Integer.parseInt(distance);
-                                    }
+                                if(!fuel.isBlank()) {
 
-                                }  }
-                            break; 
+                                    if(!fuel.isBlank()) {
+                                        if (fuel.equals("Diesel")) {
+                                            // Calcul pour bus diesel
+                                            resultat += 1100 * Integer.parseInt(distance);
+                                        }
+                                        if (fuel.equals("electric")) {
+                                            // Calcul pour bus electric
+                                            resultat += 200 * Integer.parseInt(distance);
+                                        }
+
+                                    }
+                                }
+                            break;
+                            default:
+                                break;
                         }
 
                             break;
