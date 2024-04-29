f3983f5ae5b06f08186e8e71d1d8eefef7052354
adeldzz <adnane.zemouri@etu.univ-lyon1.fr>
2024-04-29 08:42:07 +0200

    oups checkstyle

--- back/src/main/java/com/greentracer/app/dao/JourneeDao.java
+++ back/src/main/java/com/greentracer/app/dao/JourneeDao.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
